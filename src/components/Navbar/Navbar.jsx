8d8b9171418ab05ab1589a99b256c05a84d03222
yator <cherutichfelix33@gmail.com>
2024-11-26 15:33:33 +0300

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,51 +8,52 @@ import DarkMode from './DarkMode'
 
 
 
-// const Menu = [
-//   {
-//     id: 1,
-//     name: "Home",
-//     link: "/#",
-//   },
-//   {
-//     id: 2,
-//     name: "Top Rated",
-//     link: "/#services",
-//   },
-//   {
-//     id: 3,
-//     name: "Kids Wear",
-//     link: "/#",
-//   },
-//   {
-//     id: 3,
-//     name: "Mens Wear",
-//     link: "/#",
-//   },
-//   {
-//     id: 3,
-//     name: "Electronics",
-//     link: "/#",
-//   },
-// ];
 
-// const DropdownLinks = [
-//   {
-//     id: 1,
-//     name: "Trending Products",
-//     link: "/#",
-//   },
-//   {
-//     id: 2,
-//     name: "Best Selling",
-//     link: "/#",
-//   },
-//   {
-//     id: 3,
-//     name: "Top Rated",
-//     link: "/#",
-//   },
-// ];
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Electronics",
+    link: "/#",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
 
 
 
